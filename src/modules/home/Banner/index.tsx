24dfbfa9b070eebc73e302556b84ba2e5befcfee
Gabriel Esnack <gabriel_esnack@hotmail.com>
2021-05-03 16:40:36 -0300

    adicionado animate scroll

--- src/modules/home/Banner/index.tsx
+++ src/modules/home/Banner/index.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import styled from 'styled-components';
 import StyledBanner from '@components/Banners/';
 import BannerTitle from '~/components/Banners/BannerTitle';
 import BannerDescription from '~/components/Banners/BannerDescription';
 import BannerAvatar from '~/components/Banners/BannerAvatar';
 import Badge from '~/components/Badges';
-
 import { StyledBannerGrid, StyledBannerButton } from './style';
+import { Element, animateScroll as scrollSpy, scroller } from 'react-scroll'
+
+
 
 import Avatar from '@assets/gabrielesnack.jpg';
 
 export default () => {
+
+  const scrollTo = (el: string) => {
+    scroller.scrollTo(el, {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart'
+    })
+  }
+
   return (
     <StyledBanner as="section" bg="#222" aria-label="Banner">
       <StyledBannerGrid aria-label="Main content about Gabriel Esnack">
@@ -22,7 +32,7 @@ export default () => {
         <BannerDescription className="description">
           I am an enthusiast about technology and I would intend to explore several technologies to create or developer what is beyond my understanding.
         </BannerDescription>
-        <StyledBannerButton as="a" href="#PROJECTS" aria-label="Click here to see more informations about Gabriel Esnack">
+        <StyledBannerButton onClick={ () => scrollTo('PROJECTS')} aria-label="Click here to see more informations about Gabriel Esnack">
           Show More
         </StyledBannerButton>
       </StyledBannerGrid>
