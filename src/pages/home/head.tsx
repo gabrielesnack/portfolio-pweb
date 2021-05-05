3be2bd6720e19b4521d092d3b45de2d2444017c4
Gabriel Esnack <gabriel_esnack@hotmail.com>
2021-05-04 21:50:00 -0300

    atualizado metadata

--- src/pages/home/head.tsx
+++ src/pages/home/head.tsx
@@ -6,12 +6,26 @@ const Head = () => {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
-      <meta
-        name="description"
-        content="Web site created using create-react-app"
-      />
+
       <link rel="apple-touch-icon" href="%PUBLIC_URL%/img/logo192.png" />
+      
       <title>Gabriel Esnack</title>
+      <meta name="description" content="Portfolio do gabriel" />
+      <meta name="author" content="Gabriel Metadata" />
+      <meta name="keywords" content="Batata-frita, Pão de queijo" />
+
+      <meta property="og:type" content="page" />
+      <meta property="og:title" content="Gabriel Esnack" />
+      <meta property="og:image" content="" />
+      <meta property="og:description" content="Adicionei Metadata" />
+
+      <meta property="article:author" content="Gabriel Esnack" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@" />
+      <meta name="twitter:title" content="Gabriel Esnack" />
+      <meta name="twitter:creator" content="@Gabriel Esnack" />
+      <meta name="twitter:description" content="Gabriel Esnack" />
     </head>
   )
 }
--- src/pages/metadata/head.tsx
+++ src/pages/metadata/head.tsx
@@ -6,13 +6,26 @@ const Head = () => {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
-      <meta
-        title="titulo"
-        name="descrição"
-        content="Teste de metadata"
-      />
+
       <link rel="apple-touch-icon" href="%PUBLIC_URL%/img/logo192.png" />
-      <title>Trocou titulo</title>
+      
+      <title>Gabriel Esnack - MetaData</title>
+      <meta name="description" content="Portfolio do gabriel - Metadata" />
+      <meta name="author" content="Gabriel Metadata" />
+      <meta name="keywords" content="Batata-frita, Pão de queijo" />
+
+      <meta property="og:type" content="page" />
+      <meta property="og:title" content="Gabriel Esnack - MetaData" />
+      <meta property="og:image" content="" />
+      <meta property="og:description" content="Adicionei Metadata" />
+
+      <meta property="article:author" content="Gabriel Esnack - Metadata" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@" />
+      <meta name="twitter:title" content="Gabriel Esnack - Metadata" />
+      <meta name="twitter:creator" content="@Gabriel Esnack" />
+      <meta name="twitter:description" content="Gabriel Esnack - Metadata" />
     </head>
   )
 }
